e0e6394fc0fa79b76da717569c1f41906d26ccec
Yosef-Solomon19 <ysolomon@terpmail.umd.edu>
2021-02-24 15:41:06 -0500

    Doodle-v2: Add doodle, platforms, platform class

--- public/doodle/script.js
+++ public/doodle/script.js
@@ -1,12 +1,50 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const grid = document.querySelector('.grid')
-    const doodler = document.createElement('div')
+  const grid = document.querySelector('.grid')
+  const doodler = document.createElement('div')
+  let doodlerLeftSpace = 50;
+  let doodlerBottomSpace = 150;
+  let platformCount = 5;
+	let isGameOver = false;	
 
-		function createDoodler() {
-			grid.appendChild(doodler)
-			doodler.classList.add('doodler')
-			doodler.style.left = 'px';
+	function createDoodler() {
+		grid.appendChild(doodler)
+		doodler.classList.add('doodler')
+		doodler.style.left = doodlerLeftSpace + 'px'
+		doodler.style.bottom = doodlerBottomSpace + 'px'
 
-  }
-  createDoodler()
+	}
+
+	class Platform {
+		constructor(newPlatBottom) {
+			this.bottom = newPlatBottom 
+			this.left = Math.random() * 315
+			this.visual = document.createElement('div')
+
+			const visual = this.visual;
+			visual.classList.add('platform')
+			visual.style.left = this.left + 'px'
+			visual.style.bottom = this.bottom + 'px'
+			grid.appendChild(visual)
+			
+		}
+	}
+
+	function createPlatforms() {
+		for (let i =0; i < platformCount; i++) {
+			let platformGap = 600 / platformCount;
+			let newPlatBottom = 100 + i * platformGap;
+			let newPlatform = new Platform(newPlatBottom)
+		}
+	}
+
+	function start() {
+		if(!isGameOver) {
+			createDoodler()
+			createPlatforms()
+		}
+	}
+	// Attact a button to start
+	start()
+
+
 })
