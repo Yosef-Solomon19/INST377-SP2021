256bd7968d6f706e28a4fe840db7b209abf20576
Yosef-Solomon19 <ysolomon@terpmail.umd.edu>
2021-03-14 14:35:22 -0400

    Lab5-v5: Add displayMarker function Added addEventListener with the displayMarker

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -12,33 +12,33 @@ function mapInit() {
     accessToken: 'pk.eyJ1Ijoiam9lMTIxMiIsImEiOiJja202cGF1MW8wMWExMnJvMmp5OHdxM3d4In0.lDELUdeujTHCGEySWvQtrA' 
   }).addTo(mymap);
   
-
-  
   console.log('mymap',mymap);
 
   return mymap;
 }
 
-async function dataHandler(mapObjectFromFunction) {
+async function dataHandler(mapFromLeaflet) {
   // use your assignment 1 data handling code here
   // and target mapObjectFromFunction to attach markers
+  const form = document.querySelector('.search-form');
   const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
   const request = await fetch(endpoint); // Fetch request
   const zipCode = await request.json(); // Empty array, this replaces
   // the race condition promise chain from the original tutorial
+  
 
   function findMatches(numberToMatch, zipCode) {
     return zipCode.filter((restaurants) => {
-      const regex = new RegExp(numberToMatch, "gi");
+      const regex = new RegExp(numberToMatch, 'gi');
       return restaurants.zip.match(regex);
     });
   }
 
   function displayMatches(event) {
-    const matchArray = findMatches(event.target.value, zipCode);    
+    const matchArray = findMatches(event.target.value, zipCode);
     const html = matchArray
       .map((restaurants) => {
-        const regex = new RegExp(event.target.value, "gi");
+        const regex = new RegExp(event.target.value, 'gi');
         const restaurantName = restaurants.name.replace(regex,`<span class="h1">${event.target.value}</span>`); // Highlights the restaurants name
         return `
           <li>
@@ -49,14 +49,27 @@ async function dataHandler(mapObjectFromFunction) {
 				  </li>
 			  `;
       }).join('');
-      recommendations.innerHTML = html;
+      recommendations.innerHTML = html; }
+  
+  function displayMarker() {
+    matchArray.forEach((item) => {
+      const longLat = item.geocoded_column_1.coordinates;
+      console.log('markerLongLat', longLat[0], longLat[1]);
+      const marker = L.marker([longLat[1], longLat[0]]).addTo(mapFromLeaflet);
+    });
   }
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log('hi');
+    displayMarker(event);
+  });
   
 
   const searchInput = document.querySelector('.typehead');
   const recommendations = document.querySelector('.recommendations');
 
-  searchInput.addEventListener('change', displayMatches);
+  searchInput.addEventListener('input', displayMatches);
   searchInput.addEventListener('button', (evt) => {
     displayMatches(evt);
   });
